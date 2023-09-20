f47beea9d7960a3fce24034f8e77e03df3520576
Alzamer <91850532+Alzamer@users.noreply.github.com>
2023-09-20 11:20:20 +0200

    Add files via upload

---
+++ 05317-medium-lastindexof/index.ts
@@ -0,0 +1,42 @@
+/*
+  5317 - LastIndexOf
+  -------
+  by jiangshan (@jiangshanmeta) #medium #array
+
+  ### Question
+
+  Implement the type version of ```Array.lastIndexOf```, ```LastIndexOf<T, U>```  takes an Array ```T```, any ```U``` and returns the index of the last ```U``` in Array ```T```
+
+  For example:
+
+  ```typescript
+  type Res1 = LastIndexOf<[1, 2, 3, 2, 1], 2> // 3
+  type Res2 = LastIndexOf<[0, 0, 0], 2> // -1
+  ```
+
+  > View on GitHub: https://tsch.js.org/5317
+*/
+
+/* _____________ Your Code Here _____________ */
+type to_arr<T, C extends any[] = []> = C['length'] extends T ? C : to_arr<T, [any, ...C]>;
+type add<T, U> = [...to_arr<T>, ...to_arr<U>]['length'];
+type substract<T, U, C extends any [] = []> = T extends U ? C['length'] : substract<T, add<U, 1>, [any, ...C]>;
+type LastIndexOf<T extends unknown[], U extends number, C extends unknown[] = []> = T extends [...infer L, infer R] ? Equal<R, U> extends true ? substract<T['length'], C['length']> : LastIndexOf<L, U, [...C, any]> : -1;
+
+/* _____________ Test Cases _____________ */
+import type { Equal, Expect } from '@type-challenges/utils'
+
+type cases = [
+  Expect<Equal<LastIndexOf<[1, 2, 3, 2, 1], 2>, 3>>,
+  Expect<Equal<LastIndexOf<[2, 6, 3, 8, 4, 1, 7, 3, 9], 3>, 7>>,
+  Expect<Equal<LastIndexOf<[0, 0, 0], 2>, -1>>,
+  Expect<Equal<LastIndexOf<[string, 2, number, 'a', number, 1], number>, 4>>,
+  Expect<Equal<LastIndexOf<[string, any, 1, number, 'a', any, 1], any>, 5>>,
+]
+
+/* _____________ Further Steps _____________ */
+/*
+  > Share your solutions: https://tsch.js.org/5317/answer
+  > View solutions: https://tsch.js.org/5317/solutions
+  > More Challenges: https://tsch.js.org
+*/
