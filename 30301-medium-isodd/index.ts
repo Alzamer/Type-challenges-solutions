4222ffcb01fbd483506f7259bbc3152af6b86780
Alzamer <91850532+Alzamer@users.noreply.github.com>
2023-11-23 18:48:46 +0100

    Add files via upload

---
+++ 30301-medium-isodd/index.ts
@@ -0,0 +1,32 @@
+/*
+  30301 - IsOdd
+  -------
+  by jiangshan (@jiangshanmeta) #medium #string
+
+  ### Question
+
+  return true is a number is odd
+
+  > View on GitHub: https://tsch.js.org/30301
+*/
+
+/* _____________ Your Code Here _____________ */
+type aux<T extends string> = T extends `${infer L}${infer R}` ? R extends '' ? L : aux<R> : never;
+type IsOdd<T extends number> = aux<`${T}`> extends '1' | '3' | '5' | '7' | '9' ? true : false;
+
+/* _____________ Test Cases _____________ */
+import type { Equal, Expect } from '@type-challenges/utils'
+
+type cases = [
+  Expect<Equal<IsOdd<2023>, true>>,
+  Expect<Equal<IsOdd<1453>, true>>,
+  Expect<Equal<IsOdd<1926>, false>>,
+  Expect<Equal<IsOdd<number>, false>>,
+]
+
+/* _____________ Further Steps _____________ */
+/*
+  > Share your solutions: https://tsch.js.org/30301/answer
+  > View solutions: https://tsch.js.org/30301/solutions
+  > More Challenges: https://tsch.js.org
+*/
